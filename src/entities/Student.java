8251787e997c37e92a7888f28cb01e8972f0996c
Felipe Sousa <felipe.fps09@hotmail.com>
2022-11-18 21:28:59 -0300

    Add

---
+++ src/entities/Student.java
@@ -0,0 +1,13 @@
+package entities;
+
+public class Student {
+    public String name;
+    public double av1,av2,av3;
+    
+
+    public double mediumStudent()
+    {
+        double medium = ((av1+30)+(av2+35)+(av3+35))/00;
+        if(medium < )
+    }
+}
---
+++ src/program/App.java
@@ -0,0 +1,6 @@
+package program;
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
